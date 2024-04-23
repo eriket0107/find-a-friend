eb4b2c196036eab235c10ac435cc5d81b69eef8e
Erik <ol.erik0107@gmail.com>
2024-04-23 20:11:29 -0300

    lint: run lint

--- src/db/migration/1712980542458-creating-relations-between-org-pet.ts
+++ src/db/migration/1712980542458-creating-relations-between-org-pet.ts
@@ -1,20 +1,33 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
-export class CreatingRelationsBetweenOrgPet1712980542458 implements MigrationInterface {
-    name = 'CreatingRelationsBetweenOrgPet1712980542458'
+export class CreatingRelationsBetweenOrgPet1712980542458
+  implements MigrationInterface
+{
+  name = 'CreatingRelationsBetweenOrgPet1712980542458'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "pet" ADD "organizationId" uuid`);
-        await queryRunner.query(`ALTER TABLE "organization" ADD CONSTRAINT "UQ_c21e615583a3ebbb0977452afb0" UNIQUE ("name")`);
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "date" SET DEFAULT '2024-04-13T03:55:43.306Z'`);
-        await queryRunner.query(`ALTER TABLE "pet" ADD CONSTRAINT "FK_a730d8e90af7ecf1b7ae6c53e6f" FOREIGN KEY ("organizationId") REFERENCES "organization"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "pet" ADD "organizationId" uuid`)
+    await queryRunner.query(
+      `ALTER TABLE "organization" ADD CONSTRAINT "UQ_c21e615583a3ebbb0977452afb0" UNIQUE ("name")`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "user" ALTER COLUMN "date" SET DEFAULT '2024-04-13T03:55:43.306Z'`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "pet" ADD CONSTRAINT "FK_a730d8e90af7ecf1b7ae6c53e6f" FOREIGN KEY ("organizationId") REFERENCES "organization"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "pet" DROP CONSTRAINT "FK_a730d8e90af7ecf1b7ae6c53e6f"`);
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "date" SET DEFAULT '2024-04-13'`);
-        await queryRunner.query(`ALTER TABLE "organization" DROP CONSTRAINT "UQ_c21e615583a3ebbb0977452afb0"`);
-        await queryRunner.query(`ALTER TABLE "pet" DROP COLUMN "organizationId"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "pet" DROP CONSTRAINT "FK_a730d8e90af7ecf1b7ae6c53e6f"`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "user" ALTER COLUMN "date" SET DEFAULT '2024-04-13'`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "organization" DROP CONSTRAINT "UQ_c21e615583a3ebbb0977452afb0"`,
+    )
+    await queryRunner.query(`ALTER TABLE "pet" DROP COLUMN "organizationId"`)
+  }
 }
--- src/db/migration/1712980854337-creating-document-id.ts
+++ src/db/migration/1712980854337-creating-document-id.ts
@@ -1,22 +1,37 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class CreatingDocumentId1712980854337 implements MigrationInterface {
-    name = 'CreatingDocumentId1712980854337'
+  name = 'CreatingDocumentId1712980854337'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "organization" ADD "cnpj" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "organization" ADD CONSTRAINT "UQ_480aaa8101e24b3d4e4d874e441" UNIQUE ("cnpj")`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "cpf" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_a6235b5ef0939d8deaad755fc87" UNIQUE ("cpf")`);
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "date" SET DEFAULT '2024-04-13T04:00:55.221Z'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "organization" ADD "cnpj" character varying NOT NULL`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "organization" ADD CONSTRAINT "UQ_480aaa8101e24b3d4e4d874e441" UNIQUE ("cnpj")`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "cpf" character varying NOT NULL`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD CONSTRAINT "UQ_a6235b5ef0939d8deaad755fc87" UNIQUE ("cpf")`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "user" ALTER COLUMN "date" SET DEFAULT '2024-04-13T04:00:55.221Z'`,
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "date" SET DEFAULT '2024-04-13'`);
-        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_a6235b5ef0939d8deaad755fc87"`);
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "cpf"`);
-        await queryRunner.query(`ALTER TABLE "organization" DROP CONSTRAINT "UQ_480aaa8101e24b3d4e4d874e441"`);
-        await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "cnpj"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ALTER COLUMN "date" SET DEFAULT '2024-04-13'`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "user" DROP CONSTRAINT "UQ_a6235b5ef0939d8deaad755fc87"`,
+    )
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "cpf"`)
+    await queryRunner.query(
+      `ALTER TABLE "organization" DROP CONSTRAINT "UQ_480aaa8101e24b3d4e4d874e441"`,
+    )
+    await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "cnpj"`)
+  }
 }
