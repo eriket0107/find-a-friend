bc9181beca5192407bbd4cfd5ca90d5844ec137e
Erik <ol.erik0107@gmail.com>
2024-04-25 21:06:30 -0300

    refact: register pet use case test

--- src/use-cases/register-pets-use-case/index.spec.ts
+++ src/use-cases/register-pets-use-case/index.spec.ts
@@ -1,8 +1,9 @@
-import { randomUUID } from 'crypto'
 import { beforeEach, describe, expect, it } from 'vitest'
 
 import { InMemoryOrganizationRepository } from '@/repositories/in-memory/in-memory-organization'
 import { InMemoryPetRepository } from '@/repositories/in-memory/in-memory-pet'
+import { makeOrganization } from '@/tests/makeOrg'
+import { makePet } from '@/tests/makePet'
 
 import { OrgNotFoundError } from '../errors/org-not-found-error'
 import { RegisterPetsUseCase } from '.'
@@ -19,52 +20,29 @@ describe('Register Pet Use Case', async () => {
   })
 
   it('shuld be able to register a pet in organization', async () => {
-    const address = {
-      city: 'Rio de Janeiro',
-      state: 'RJ',
-      country: 'BRA',
-      zipCode: '22790710',
-      street: 'Alfredo Balthazar da silveira',
-    }
+    const fakeOrg = makeOrganization()
+    const organization = await organizationRepository.create(fakeOrg)
 
-    const organization = await organizationRepository.create({
-      address,
-      id: randomUUID(),
-      cnpj: '89656977000175',
-      email: 'organization@email.com',
-      name: 'Organization',
-      password: '123456',
-      phone: '21999132991',
-    })
+    const fakePet = await petRepository.create({ ...makePet(), organization })
 
     const { pet } = await sut.execute({
-      age: 4,
-      breed: 'Golden Retriver',
-      description:
-        'Lorem Ipsum is Lorem Ipsum Lorem Ipsum is Lorem Ipsum Lorem Ipsum is Lorem Ipsum',
-      name: 'Dog',
-      photo: 'teste.png',
-      traits: ['Brincalhão', 'Fofo'],
-      size: 'small',
+      ...fakePet,
       organizationId: organization.id,
     })
 
-    expect(pet.name).toEqual('Dog')
+    expect(pet.name).toEqual(fakePet.name)
     expect(pet).toEqual(expect.objectContaining(pet))
     expect(pet.organization).toEqual(expect.objectContaining(organization))
   })
 
   it('should not be possible to register a pet without an organization', async () => {
+    const organization = makeOrganization()
+
+    const fakePet = await petRepository.create({ ...makePet(), organization })
+
     await expect(
       sut.execute({
-        age: 4,
-        breed: 'Golden Retriver',
-        description:
-          'Lorem Ipsum is Lorem Ipsum Lorem Ipsum is Lorem Ipsum Lorem Ipsum is Lorem Ipsum',
-        name: 'Dog',
-        photo: 'teste',
-        traits: ['Brincalhão', 'Fofo'],
-        size: 'small',
+        ...fakePet,
         organizationId: '1',
       }),
     ).rejects.to.toBeInstanceOf(OrgNotFoundError)
