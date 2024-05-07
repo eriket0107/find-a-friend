10f2b46cd903e56af3ff6249b7e9313384381259
Erik <ol.erik0107@gmail.com>
2024-05-07 06:39:07 -0300

    fix: organization factory with passwordHandler

--- src/use-cases/fatories/make-organizations.ts
+++ src/use-cases/fatories/make-organizations.ts
@@ -1,12 +1,16 @@
 import { TypeOrmOrganizationRepostory } from '@/repositories/typeorm/typeorm-organization-repository'
+import { PasswordHandler } from '@/services/passwordHandler'
 
 import { CreateOrganizationUseCase } from '../create-organization-use-case'
 
 export const makeOrganization = () => {
   const organizationRepository = new TypeOrmOrganizationRepostory()
 
+  const passwordHandle = new PasswordHandler()
+
   const createOrganizationUseCase = new CreateOrganizationUseCase(
     organizationRepository,
+    passwordHandle,
   )
 
   return createOrganizationUseCase
